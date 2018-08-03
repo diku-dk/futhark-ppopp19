55ba28fc6e1cc1e0e1c1d72de6037288c909fdf8
Troels Henriksen <athas@sigkill.dk>
2018-08-03 23:26:55 +0200

    This graph is crap.

--- tools/bulk-impact-plot.py
+++ tools/bulk-impact-plot.py
@@ -28,7 +28,7 @@ programs = [ # Novel ones
             ("Backprop",
              "backprop",
              [("small", "backprop-data/small.in"),
-              ("small", "backprop-data/medium.in")]),
+              ("medium", "backprop-data/medium.in")]),
             ("LavaMD",
              "lavaMD",
              [("27x64x30", "lavaMD-data/27_64_30.in"),
@@ -84,7 +84,7 @@ plt.rc('text', usetex=True)
 grey='#aaaaaa'
 
 i = 0
-plt.figure(figsize=(8, 1))
+plt.figure(figsize=(10, 1))
 
 for (program_name, info) in program_plots:
     print('Plotting {}...'.format(program_name))
@@ -94,10 +94,12 @@ for (program_name, info) in program_plots:
     untuned_speedups = np.array(ref_runtimes)/np.array(untuned_runtimes)
     tuned_speedups = np.array(ref_runtimes)/np.array(tuned_runtimes)
 
+    plt.gca().get_yaxis().set_visible(False)
+
     ind = np.arange(len(ref_runtimes))
     plt.gca().grid(True, axis='y', linestyle='-', linewidth='2', color='grey')
     plt.gca().set_title(program_name)
-    plt.gca().set_xticks(ind + width / 2)
+    plt.gca().set_xticks(ind + width)
     plt.tick_params(
         axis='x',          # changes apply to the x-axis
         which='both',      # both major and minor ticks are affected
@@ -113,20 +115,45 @@ for (program_name, info) in program_plots:
                           color='#ff7c4c', hatch='*', zorder=3, label="Autotuned")
     ymax = plt.ylim()[1]
 
-    # if ymax > 3:
-    #     plt.gca().set_yticks(np.arange(np.ceil(plt.ylim()[1])))
-    # else:
-    #     plt.gca().set_yticks(np.arange(np.ceil(plt.ylim()[1]/0.5))*0.5)
     notch = ymax/30
 
-    for (dataset_name, r, ref) in zip(dataset_names, notuned_rects, ref_runtimes):
-        plt.text(r.get_x()+r.get_width(), r.get_y()-notch,
-                 "{}\n${}ms$".format(dataset_name, int(ref/1000)),
-                 ha='center', va='top')
+    for (dataset_name, r1, r2, ref) in zip(dataset_names, notuned_rects,
+                                           tuned_rects, ref_runtimes):
+        if ref > 1000000:
+            time = "$%.2fs$" % (ref/1000000)
+        elif ref < 5000:
+            time = "$%.2fms$" % (ref/1000)
+        else:
+            time = "${}ms$".format(int(ref/1000))
+
+        plt.text(r1.get_x()+r1.get_width(), r1.get_y()-6*notch,
+                 "{}".format(dataset_name),
+                 ha='center', va='top', rotation=360-25)
+        plt.text(r1.get_x()+r1.get_width(), r1.get_y()-notch,
+                 time, ha='center', va='top')
+
+        r1_ypos = max(4*notch, r1.get_y()+r1.get_height()+3*notch)
+        r2_ypos = max(4*notch, r2.get_y()+r2.get_height()+3*notch)
+
+        if np.abs(r2_ypos - r1_ypos) < 4*notch:
+            if r1_ypos > 4*notch:
+                r1_ypos = r2_ypos - 4*notch
+            else:
+                r2_ypos = r1_ypos + 4*notch
+
+        if r1.get_height() > r2.get_height() and r1_ypos < r2_ypos:
+            (r1_ypos, r2_ypos) = (r2_ypos, r1_ypos)
+
+        plt.text(r1.get_x(), r1_ypos,
+                 "%.1f$\\times$" % (round(r1.get_height()*10)/10),
+                 ha='left', va='top')
+        plt.text(r2.get_x(), r2_ypos,
+                 "%.1f$\\times$" % (round(r2.get_height()*10)/10),
+                 ha='left', va='top')
 
     i += 1
 
-plt.legend(bbox_to_anchor=(1,-0.7), loc='lower center', ncol=2, borderaxespad=0.)
+plt.legend(bbox_to_anchor=(9.4,-1), loc='lower right', ncol=2, borderaxespad=0.)
 plt.rc('text')
 print('Saving {}...'.format(outputfile))
 plt.savefig(outputfile, bbox_inches='tight')
