d8319cc89935fec512e1310ede05cb19c4841d2f
Troels Henriksen <athas@sigkill.dk>
2018-08-09 12:00:03 +0200

    Simplify the LocVolCalib plot.

--- tools/LocVolCalib-plot.py
+++ tools/LocVolCalib-plot.py
@@ -24,7 +24,7 @@ def datasets():
 
 def plot_results(dataset):
     data_files, data_names = zip(*datasets())
-    plt.gca().grid(True, axis='y', linestyle='-', linewidth='2', color='grey')
+    plt.gca().grid(True, axis=True, linestyle='-', linewidth='2', color='grey')
 
     def plot_it(progname, fname, offset, **kwargs):
         j = json.load(open(fname))
@@ -34,32 +34,22 @@ def plot_results(dataset):
         ys = [np.mean(all_runtimes[df]['runtimes'])/1000]
         return plt.bar(offset + np.arange(len(ys)) * 8 + 10, ys, **kwargs)[0]
 
-    plt.tick_params(
-        axis='x',          # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom='off',      # ticks along the bottom edge are off
-        top='off',         # ticks along the top edge are off
-        labelbottom='off') # labels along the bottom edge are off
+    plt.yticks([], [])
+    plt.xticks([], [])
 
     rects = [plot_it('LocVolCalib', 'results/LocVolCalib-moderate.json', 0,
                      label="moderate flattening",
                      color='#abedaf', hatch='/'),
-             plot_it('LocVolCalib-partridag', 'results/LocVolCalib-partridag-moderate.json', 1,
-                     label="moderate flattening (only parallel tridag)",
-                     color='#adc3ff', hatch='+'),
-             plot_it('LocVolCalib', 'results/LocVolCalib-incremental.json', 2,
+             plot_it('LocVolCalib', 'results/LocVolCalib-incremental.json', 1,
                      label="incremental flattening",
-                     color='#ffcebf', hatch='\\'),
-             plot_it('LocVolCalib-partridag', 'results/LocVolCalib-partridag-incremental.json', 3,
-                     label="incremental flattening (only parallel tridag)",
                      color='#e2f442', hatch='X'),
-             plot_it('LocVolCalib-partridag', 'results/LocVolCalib-partridag-incremental-tuned.json', 4,
-                     label="incremental flattening (only parallel tridag; auto-tuned)",
+             plot_it('LocVolCalib', 'results/LocVolCalib-incremental-tuned.json', 2,
+                     label="incremental flattening (auto-tuned)",
                      color='#ff7c4c', hatch='*'),
-             plot_it('LocVolCalib', 'results/LocVolCalib-finpar-OutParOpenCLMP.json', 5,
-                     label="FinPar (only outer parallelism)",
+             plot_it('LocVolCalib', 'results/LocVolCalib-finpar-OutParOpenCLMP.json', 3,
+                     label="FinPar (outer parallelism)",
                      color='#54c3f2', hatch='o'),
-             plot_it('LocVolCalib', 'results/LocVolCalib-finpar-AllParOpenCLMP.json', 6,
+             plot_it('LocVolCalib', 'results/LocVolCalib-finpar-AllParOpenCLMP.json', 4,
                      label="FinPar (all parallelism)",
                      color='#8cffd4', hatch='.')]
     reference = rects[-1].get_height()
@@ -75,19 +65,18 @@ def plot_results(dataset):
         ylim = 6000
 
     for r in rects:
-        plt.text(r.get_x()-0.1, r.get_height()+ylim/60, "$\\times$%.1f" % (r.get_height()/reference))
+        plt.text(r.get_x(), r.get_height()+ylim/60, "%.1f" % (r.get_height()/reference))
     plt.margins(0.1, 0)
     plt.xlabel(dataset)
     plt.ylim(ymax=ylim)
 
-plt.figure(figsize=(12,3))
+plt.figure(figsize=(6,1.5))
 plt.subplot(1,3,1)
 plot_results('small')
 plt.subplot(1,3,2)
 plot_results('medium')
 plt.subplot(1,3,3)
 plot_results('large')
-plt.legend(bbox_to_anchor=(-0.8,-0.2), loc='upper center', ncol=2, borderaxespad=0.)
-plt.tight_layout()
+plt.legend(bbox_to_anchor=(-0.7,-0.2), loc='upper center', ncol=2, borderaxespad=0.)
 
 plt.savefig("LocVolCalib-runtimes.pdf", bbox_inches='tight')
